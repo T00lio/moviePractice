702ef897a73d02ebf0080970d2df8bc801e541f2
Tulio Salvatierra <salvacorp@gmail.com>
2023-08-01 21:32:25 -0500

    fixed route params

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,41 +1,42 @@
-import styles from '../MovieDetails.module.css';
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { get } from '../utils/httpClient';
-
+import styles from "../MovieDetails.module.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { get } from "../utils/httpClient";
 
 export function MovieDetails() {
-    const { movieId } = useParams();
-    const [movie, setMovie] = useState(null);
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
 
-    useEffect(() => 
-     get("/movie/" + movieId).then((data) => {
-        setMovie(data);
-        }),
-        [movieId]);
-    if (!movie) {
-        return null;
-    }
-    const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-   
+  useEffect(() => {
+    get("/movie/" + movieId).then((data) => {
+      setMovie(data);
+    });
+  }, [movieId]);
 
-    return (
+  if (!movie) {
+    return null;
+  }
+  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+  return (
     <div className={styles.detailsContainer}>
-        <img className={`${styles.col} ${styles.movieImage}`} src={imageUrl} alt={movie.title} />
-        <div className={`${styles.col} ${styles.movieDetails}`}>
-            <p className={styles.firstItem}>
-                <strong>Title:</strong> {movie.title}
-            </p>
-            <p>
-                <strong>Genre:</strong>
-                {movie.genres.map(genre => genre.name).join(", ")}
-            </p>
-            <p>
-                <strong>Description:</strong> {movie.overview}
-            </p>
-
-
-        </div>
+      <img
+        className={`${styles.col} ${styles.movieImage}`}
+        src={imageUrl}
+        alt={movie.title}
+      />
+      <div className={`${styles.col} ${styles.movieDetails}`}>
+        <p className={styles.firstItem}>
+          <strong>Title:</strong> {movie.title}
+        </p>
+        <p>
+          <strong>Genre:</strong>
+          {movie.genres.map((genre) => genre.name).join(", ")}
+        </p>
+        <p>
+          <strong>Description:</strong> {movie.overview}
+        </p>
+      </div>
     </div>
-    )
+  );
 }
